21f0f7110477b0df028362815f1daf4a75158030
ichizero <30754019+ichizero@users.noreply.github.com>
2018-11-21 23:44:08 +0900

    Fix regex

--- face_recognizer.py
+++ face_recognizer.py
@@ -55,7 +55,7 @@ class FaceRecognizer:
         """ Loads known faces. """
         faces_path = Path(self.faces_path)
 
-        img_path_list = [p for p in faces_path.glob("*") if re.search(r"^[^\.]*\.(png|jpg|py)$", str(p))]
+        img_path_list = [p for p in faces_path.glob("*") if re.search(r"^[^\.]*\.(png|jpg|py)$", str(p.name))]
 
         known_faces = []
         known_names = []
